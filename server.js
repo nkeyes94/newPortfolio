fbb294c5488a5ee2de31214b949ad2366963f577
Nathan Keyes <n.keyes1994@gmail.com>
2019-09-28 11:10:57 -0400

    PORT Configured wrong, corrected issue

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var express = require("express");
 var exphbs = require("express-handlebars");
 
 // * Express config
-var PORT = process.env.port || 8080;
+var PORT = process.env.PORT || 8080;
 var app = express();
 
 // * Middleware config
